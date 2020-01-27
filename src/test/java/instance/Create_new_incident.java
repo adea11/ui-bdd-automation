1d0fa506b8ed04fc2b8a47e743514fddcff0f4fb
ade.ajayi <ade.ajayi@sainsbury.co.uk>
2020-01-27 10:00:42 +0000

    indent

--- src/test/java/instance/Create_new_incident.java
+++ src/test/java/instance/Create_new_incident.java
@@ -22,7 +22,6 @@ public class Create_new_incident {
 
     @After
     public void afterEachTest() {
-
         webDriverProvider.driver.quit();
     }
 
@@ -30,7 +29,6 @@ public class Create_new_incident {
     @Given("^Go to home page$")
     public void go_to_home_page() throws Throwable {
         webDriverProvider.driver.findElement(By.xpath("//*[@id=\"]")).click();
-
     }
 
 
@@ -38,14 +36,12 @@ public class Create_new_incident {
     @When("^Login with username as \"([^\"]*)\" and password as \"([^\"]*)\"$")
     public void login_with_username_as_and_password_as(String arg1, String arg2) throws Throwable {
         webDriverProvider.driver.findElement(By.name("email")).sendKeys("a");
-
     }
 
 
     @When("^Verify \"([^\"]*)\" to confirm that login is successful$")
     public void verify_to_confirm_that_login_is_successful(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-
     }
 
     @When("^Navigate 'to Create Incident' page$")
@@ -53,55 +49,43 @@ public class Create_new_incident {
         webDriverProvider.driver.findElement(By.id("search_field")).sendKeys("so54fpp");
         webDriverProvider.driver.findElement(By.cssSelector(".search-button")).click();
         Thread.sleep(5000);
-
     }
 
     @When("^Complete mandatory fields by entering \"([^\"]*)\" into Caller field and \"([^\"]*)\" into Short Description field$")
     public void complete_mandatory_fields_by_entering_into_Caller_field_and_into_Short_Description_field(String arg1, String arg2) throws Throwable {
         String package_title = webDriverProvider.driver.findElement(By.cssSelector(".package-title ")).getText();
-
         Assert.assertEquals("factsheet", package_title);
-
-
     }
 
     @When("^Add \"([^\"]*)\" in Description field$")
     public void add_in_Description_field(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-
     }
 
     @When("^Submit Incident$")
     public void submit_Incident() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-
     }
 
     @When("^Confirm that incident has been successfully created with \"([^\"]*)\"$")
     public void confirm_that_incident_has_been_successfully_created_with(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-
     }
 
     @When("^Click User menu$")
     public void click_User_menu() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-
     }
 
     @When("^Click \"([^\"]*)\" to sign out$")
     public void click_to_sign_out(String arg1) throws Throwable {
         String package_title = webDriverProvider.driver.findElement(By.cssSelector(".package-title ")).getText();
-
         Assert.assertEquals("factsheet", package_title);
-
-
     }
 
     @Then("^Verify that user has succesfully logged out \"([^\"]*)\"$")
     public void verify_that_user_has_succesfully_logged_out(String arg1) throws Throwable {
         String package_title = webDriverProvider.driver.findElement(By.cssSelector(".package-title ")).getText();
-
         Assert.assertEquals("factsheet", package_title);
     }
 }
